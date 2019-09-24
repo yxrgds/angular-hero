638aa86fb36c9b523f4af54de3ebfb05cfbe51b9
杨筱 <yangxiao3@xiaomi.com>
2019-09-24 21:11:02 +0800

    add some

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -18,7 +18,8 @@ export class HeroDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private heroService: HeroService,
     private location: Location
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.getHero();
@@ -29,4 +30,7 @@ export class HeroDetailComponent implements OnInit {
     this.heroService.getHero(id).subscribe(hero => this.hero = hero);
   }
 
+  goBack(): void {
+    this.location.back();
+  }
 }
